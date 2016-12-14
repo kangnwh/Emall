893c5dd48e701bc706e9ba40021448a840909404
qlrual <501_5@163.com>
2016-12-14 20:39:24 +0800

    add config.py

---
+++ webapp/config.py
@@ -0,0 +1,27 @@
+import webapp as webapp
+
+# DB INFORMATION
+META_DB = {
+    'DB_TYPE': 'mysql',
+    'DB_HOST': '106.14.31.132',
+    'DB_PORT': 3306,
+    'DB_USER': 'emall',
+    'DB_PASSWORD': 'dbpw4LS!',
+    'DB_NAME': 'emall'
+
+}
+
+# DEBUF INFORMATION
+DEBUG = True
+
+# SECURITY
+SECRET_KEY = 'Du#Sy6j@peK~egjvrC$90nu7eK2K_#iA@CI0)mhJwkf2Pk_AWZakp$a&(FM*'
+
+# APPLICATION NAME
+APP_NAME = 'Emall'
+
+# BIND INFORMAITON
+HOST = '106.14.31.132'
+PORT = 5002
+
+#ALLOWED_EXTENSIONS=set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','TXT', 'PDF', 'PNG', 'JPG', 'JPEG', 'GIF'])
