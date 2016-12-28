3a1f996525e9a6e83bf8c062857d371c6c8e077f
kangnwh <hnzz_kn@163.com>
2016-12-28 09:29:15 +0800

    user feedback for order

--- webapp/static/js/myjs.js
+++ webapp/static/js/myjs.js
@@ -12,3 +12,15 @@ function initSwitch(s, stat, func) {
     //    }
     //});
 }
+
+
+String.format = function() {
+    if (arguments.length == 0)
+        return null;
+    var str = arguments[0];
+    for ( var i = 1; i < arguments.length; i++) {
+        var re = new RegExp('\\{' + (i - 1) + '\\}', 'gm');
+        str = str.replace(re, arguments[i]);
+    }
+    return str;
+};
