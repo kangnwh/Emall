e830a757be70a854ec719e5935169aa66a087e14
kangnwh <hnzz_kn@163.com>
2017-01-16 14:32:19 +0800

    start script

--- all_start.sh
+++ all_start.sh
@@ -1,7 +1,14 @@
 
 pid=`ps -ef|grep -i "python ./run.py"|cut -f 3 -d' '`
 for id in $pid;do
-kill -9 $id
+    p=`ps -fp $id`
+    echo "Kill\r\n $p ..."
+    kill -9 $id
+    if [ $? -eq 0 ];then
+        echo "Kill successfully"
+    else
+        echo "Kill failed"
+    fi;
 done;
 
 nohup python ./run.py emall &
